53fbd1229950509b911be3b4c2281ffcc7f61c07
Faithtorian Goins <faithtoriangoins@gmail.com>
2023-09-12 18:25:49 -0500

    fixed compile errors

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -5,10 +5,14 @@ namespace ChristmasWithTypes
     {
         public string[] Presents;
         public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public int? TreeHeight { get; set; } //TODO Make the Height property nullable
 
         //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        public enum Day { Monday, Tuesday, Wenseday, Thursday, Friday, Saturday };
 
     }
+    // vlaue types are stored in a stack
+    //value types are the actual value
+    //Ctrl and clicking value type shows you the value 
+    //highlight code  press ALt and down arrow moves your code.
 }
